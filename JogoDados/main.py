6ff7bba4503e4b57b6b7208e094ea5182a990528
Gabriel <Gabriel.Carvalho@fadami.com.br>
2021-06-24 10:24:31 -0300

    Criado jogo de dados
    Criado jogo de dados

---
+++ JogoDados/main.py
@@ -0,0 +1,5 @@
+from rolaDados import Dado
+
+jogo = Dado(tipo=6,qtd=6)
+
+jogo.RolaDados()
---
+++ JogoDados/rolaDados.py
@@ -0,0 +1,31 @@
+from random import randint
+from time import sleep
+
+class Dado:
+    def __init__(self, tipo=1, qtd=1):
+        self.tipo = tipo
+        self.qtd = qtd
+        self.dados = [4, 6, 8, 10, 12, 20, 100]
+        self.total = 0
+
+
+    def RolaDados(self):
+        if self.tipo in self.dados:
+            print(f'Você selecionou {self.qtd} D-{self.tipo}...')
+            print()
+            sleep(2)
+            print('Rolando...')
+            print()
+            for i in range(0, self.qtd):
+                valor = randint(1, self.tipo)
+                print(f'[{valor}]', end=' ')
+                sleep(1)
+                #self.dados.append(valor)
+                self.total = self.total + valor
+            print()
+            print()
+            print(f'Total: {self.total}')
+
+        else:
+            print('Digite um tipo de dado válido...')
+
