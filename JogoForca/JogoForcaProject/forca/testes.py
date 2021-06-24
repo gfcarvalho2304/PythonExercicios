b9d5fb65c449961adfc8e4b60366c3d5b1b45d70
Gabriel <Gabriel.Carvalho@fadami.com.br>
2021-06-24 09:14:32 -0300

    Organização diretorios
    Organização diretorios

--- JogoForca/JogoForcaProject/forca/testes.py
+++
@@ -1,9 +0,0 @@
-from forca.cadastro import *
-palavras = 'palavras.txt'
-teste=[]
-palavra='TESTE'
-a = open(palavras,'rt')
-for linha in a:
-    teste.append(linha.replace('\n', ''))
-
-print(teste)
